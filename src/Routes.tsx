36683c6180611374eb7536de526ad3d206a96ce8
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2022-06-05 10:19:03 +0700

    added view about

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,12 @@
 import { Routes as ReactRoutes, Route, BrowserRouter } from "react-router-dom"
-import { Home } from "views"
+import { About, Home } from "views"
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <ReactRoutes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </ReactRoutes>
     </BrowserRouter>
   )
---
+++ src/views/About.tsx
@@ -0,0 +1,25 @@
+import { illustration } from "assets"
+import clsx from "clsx"
+import { Footer, Navbar } from "components"
+import styles from "./Home.module.scss"
+
+export const About = () => {
+    return (
+        <div className={styles['home-container']}>
+            <Navbar />
+            <div className={clsx(
+                'container',
+                styles['home-content']
+            )}>
+                <h4 className="text-center fw-bold text-info mt-5" style={{ marginBottom: '92px' }}>Tentang Aplikasi Palet Warna</h4>
+                <div className="row">
+                    <div className="col-lg-5">
+                        <img src={illustration} alt="illustraiton" width={514} height={356.6} className="img-fluid" />
+                    </div>
+                    <div className="col-lg-7" style={{ textAlign: 'justify' }}>Palet warna adalah aplikasi untuk menentukan warna yang dominan dari gambar yang telah ada. Dengan adanya warna tersebut diharapkan dapat mempermudah dalam bidang disain grafis, fotografi atau pun seni rupa. Penentuan warna dalam aplikasi ini menggunakan metode pengelompokan warna dengan algoritma data mining K-means.</div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
